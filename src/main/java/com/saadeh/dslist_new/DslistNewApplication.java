7952397f2e40dd0d0dcf422c0b1f73e8b99bcc98
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-05-22 13:39:29 -0300

    project created

---
+++ src/main/java/com/saadeh/dslist_new/DslistNewApplication.java
@@ -0,0 +1,13 @@
+package com.saadeh.dslist_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saadeh/dslist_new/DslistNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadeh.dslist_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
